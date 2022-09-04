cb0b297102b422d590106d14226916479cd019cd
Ezio Lemes <eziolemes@gmail.com>
2022-09-04 17:16:48 -0400

    feat: implementação da autenticação

---
+++ src/pages/ForgotPassword.vue
@@ -0,0 +1,65 @@
+<template>
+  <q-page padding>
+    <q-form class="row justify-center" @submit.prevent="handlerForgotPassword">
+      <p class="col-12 text-h5 text-center">Reset Password</p>
+      <div class="col-md-4 col-sm-6 col-xs-10 q-gutter-y-sm">
+
+        <q-input
+          label="Email"
+          v-model="email"
+          lazy-rules
+          :rules="[val => (val && val.length > 0) || 'Email is required']"
+          type="email"
+        />
+
+        <div class="full-width q-pt-md q-gutter-y-sm">
+          <q-btn
+            label="Send Reset Email"
+            color="primary"
+            class="full-width"
+            outline
+            rounded
+            type="submit"
+          />
+
+          <q-btn
+            label="Back"
+            color="dark"
+            class="full-width"
+            rounded
+            flat
+            :to="{name: 'login'}"
+          />
+        </div>
+      </div>
+    </q-form>
+  </q-page>
+</template>
+<script>
+import { defineComponent, ref } from 'vue'
+import useNotify from 'src/composables/UseNotify'
+import useAuthUser from 'src/composables/UseAuthUser'
+
+export default defineComponent({
+  setup () {
+    const { sendPasswordRestEmail } = useAuthUser()
+    const { notifyError, notifySuccess } = useNotify()
+
+    const email = ref('')
+
+    const handlerForgotPassword = async () => {
+      try {
+        await sendPasswordRestEmail(email.value)
+        notifySuccess(`Password reset email sent to: ${email.value}`)
+      } catch (error) {
+        notifyError(error.message)
+      }
+    }
+
+    return {
+      email,
+      handlerForgotPassword
+    }
+  }
+})
+</script>
